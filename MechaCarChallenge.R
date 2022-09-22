d5780857146b1b404759079000a25f7e9a58425a
Joe Rodini <joseph.rodini@gmail.com>
2022-09-22 10:06:23 -0700

    Deliverable 2 script complete.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,21 @@
+# Deliverable 1
+
 library(dplyr)
 
-MechaCarMPG <- read.csv('MechaCar_mpg.csv') #import dataset
+MechaCarMPG <- read.csv('MechaCar_mpg.csv') #import MechaCar dataset
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarMPG) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCarMPG)) #generate summary statistics
+
+# Deliverable 2
+
+SuspensionCoil <- read.csv('Suspension_Coil.csv') #import SuspensionCoil dataset
+
+library(tidyverse)
+
+total_summary <- SuspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create summary table with multiple columns
+
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create summary table with multiple columns
+
+# Deliverable 3
