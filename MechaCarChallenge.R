4087a364d9107feac85a3c11f6a588bafc23a257
Joe Rodini <joseph.rodini@gmail.com>
2022-09-22 10:45:08 -0700

    Deliverable 3 script complete.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,17 @@ total_summary <- SuspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI)
 lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create summary table with multiple columns
 
 # Deliverable 3
+
+t.test(SuspensionCoil$PSI, mu=1500) #t-test to determine if PSI differs from population mean
+
+Lot1 <- subset(SuspensionCoil, Manufacturing_Lot=="Lot1")
+
+t.test(Lot1$PSI, mu=1500) #t-test to determine if PSI from Lot 1 differs from population mean
+
+Lot2 <- subset(SuspensionCoil, Manufacturing_Lot=="Lot2")
+
+t.test(Lot2$PSI, mu=1500) #t-test to determine if PSI from Lot 2 differs from population mean
+
+Lot3 <- subset(SuspensionCoil, Manufacturing_Lot=="Lot3")
+
+t.test(Lot3$PSI, mu=1500) #t-test to determine if PSI from Lot 3 differs from population mean
